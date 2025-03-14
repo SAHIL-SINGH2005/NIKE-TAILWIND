77cf172798feddaba1e5feaf8d1bc3d28cc4a06e
SAHIL-SINGH2005 <iffcogc34@gmail.com>
2025-03-15 01:39:35 +0530

    refactor Nav and Hero components to use dynamic navigation links and improve layout structure

--- Nike_Tailwind/src/components/Nav.jsx
+++ Nike_Tailwind/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
-
+import { navLinks } from "../constants";
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
@@ -16,8 +16,20 @@ const Nav = () => {
         </a>
         <ul className="flex-1 flex justify-center 
             items-center gap-16 mx-lg:hidden">
-
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className="font-montserrat 
+                  leading-normal text-lg text-slate-500">
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
+        <div className="hidden mx-lg:block">
+          <img src={hamburger} alt="Hamburger"
+            width={25}
+            height={25} />
+        </div>
       </nav>
     </header>
   )
--- Nike_Tailwind/src/sections/Hero.jsx
+++ Nike_Tailwind/src/sections/Hero.jsx
@@ -1,6 +1,15 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+   <section
+   id="home"
+   className="w-full 
+   flex- xl:row flex-col 
+   justify-center min-h-screen gap-10 max-container"
+   >
+    <div>
+      
+    </div>
+   </section>
   )
 }
 
