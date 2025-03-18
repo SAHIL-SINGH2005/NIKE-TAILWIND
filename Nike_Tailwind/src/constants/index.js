0ef2ba886fb4643d24d40278819ec9d0558399f9
SAHIL-SINGH2005 <iffcogc34@gmail.com>
2025-03-19 02:12:37 +0530

    add ReviewCard and ServiceCard components; implement CustomerReviews and Services sections; enhance Footer and SpecialOffer sections with dynamic content

--- Nike_Tailwind/src/constants/index.js
+++ Nike_Tailwind/src/constants/index.js
@@ -73,13 +73,14 @@ export const services = [
 export const reviews = [
     {
         imgURL: customer1,
-        customerName: 'Morich Brown',
-        rating: 4.5,
+    customerName: 'CHXUTPAGLU JI',
+        rating: -0.5,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+        
     },
     {
         imgURL: customer2,
-        customerName: 'Lota Mongeskar',
+        customerName: 'Sundar Aurat',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
