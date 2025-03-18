43d276149d513b5ce7207049fadb6dc307db6e0c
SAHIL-SINGH2005 <iffcogc34@gmail.com>
2025-03-19 01:29:53 +0530

    add SuperQuality section with dynamic content and integrate Button component; update Tailwind config for custom fonts and colors

--- Nike_Tailwind/tailwind.config.js
+++ Nike_Tailwind/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+      palanquin: ['Palanquin', 'sans-serif'],
+    },
+    colors: {
+      'slate-gray': '#708090',
+    },},
   },
   plugins: [],
 }
